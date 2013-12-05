cdca6323a31472afd0897535fd0ad213334bd64c
Franck D'agostini <franck.dagostini@gmail.com>
2013-12-05 11:09:50 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clubdetri::Application.config.secret_key_base = '7b4088acea063f134e891266ada5f4b0dd40ee8de2743009b03183cd217a63d28ee53efd7711dc4f2690e96fe3d68a9a61bc1cf3e38520472bb070d1552def27'
